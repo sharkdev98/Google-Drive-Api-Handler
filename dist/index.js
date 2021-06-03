620d32863572c306a0d6a27e1df1d00b01162f9a
Ali Hassan <sharkdev98@gmail.com>
2021-06-03 12:18:02 +0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -48296,7 +48296,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  core.setOutput("greetString","Hello World");
+  core.setOutput("greetString","Greetings! Hello World");
   main().catch(console.error);
 } catch (error) {
   core.setFailed(error.message);
