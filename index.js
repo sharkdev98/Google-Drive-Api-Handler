bfd325d6a6d8e643fa5c53075b201b8c73c61326
Ali Hassan <sharkdev98@gmail.com>
2021-06-03 11:42:55 +0500

    Google Api Service Account Sign In

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {auth, Compute} = require('google-auth-library');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,19 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  main().catch(console.error);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+async function main() {
+  const client = new Compute({
+    // Specifying the service account email is optional.
+    serviceAccountEmail: 'autoupdatingserviceaccount@autoupdates-315412.iam.gserviceaccount.com'
+  });
+  const projectId = await auth.getProjectId();
+  const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
+  const res = await client.request({url});
+  console.log(res.data);
+}
+
